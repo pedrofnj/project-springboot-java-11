e5361b544bf24f2afc31589e23415572d8620227
Pedro Felix <pedrofelixnunes.j@gmail.com>
2021-02-18 21:42:11 -0300

    Project created

---
+++ src/test/java/com/project/spring/course/SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.spring.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
